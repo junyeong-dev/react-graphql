0902132493a41d96b2b508570ac47c3d833d651c
Jun-cloud <kjy8827@gmail.com>
2021-01-02 12:02:39 +0900

    GET_MOVIES query

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    uri: "https://movieql.now.sh/"
+    uri: "//localhost:4000"
 });
 
 export default client;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,20 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+// query를 작성 ''가 아닌 ``로 작성해야함
+const GET_MOVIES = gql`
+{
+    movies {
+        id
+        medium_cover_image
+    }
+}
+`;
+
+export default () => {
+    // react-hook 을 이용하여 간단하게 쿼리를 보낼 수 있음
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    console.log(loading, error, data);
+    return <h1>Home</h1>
+};
