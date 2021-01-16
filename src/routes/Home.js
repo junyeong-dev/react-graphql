e02ac7f0fe458061fedb853824e6dd108242b3a0
Jun-cloud <kjy8827@gmail.com>
2021-01-16 14:02:06 +0900

    Refactoring

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 // query를 작성 ''가 아닌 ``로 작성해야함
 const GET_MOVIES = gql`
@@ -12,9 +14,52 @@ const GET_MOVIES = gql`
 }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 export default () => {
     // react-hook 을 이용하여 간단하게 쿼리를 보낼 수 있음
     const { loading, error, data } = useQuery(GET_MOVIES);
-    console.log(loading, error, data);
-    return <h1>Home</h1>
+    return (
+        <Container>
+            <Header>
+                <Title>Appolo</Title>
+                <Subtitle>GraphQL</Subtitle>
+            </Header>
+            { loading && <Loading>Loading</Loading> }
+            { !loading && data.movies && data.movies.map(m => <Movie key={ m.id } { ...m }/>) }
+        </Container>
+    );
 };
