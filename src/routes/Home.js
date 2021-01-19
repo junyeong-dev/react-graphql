4b3ef5d603c175ba430ddc4d388310a93b5898df
Jun-cloud <kjy8827@gmail.com>
2021-01-19 19:58:30 +0900

    Styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -49,6 +49,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
     // react-hook 을 이용하여 간단하게 쿼리를 보낼 수 있음
     const { loading, error, data } = useQuery(GET_MOVIES);
@@ -60,7 +69,13 @@ export default () => {
                 <Subtitle>GraphQL</Subtitle>
             </Header>
             {loading && <Loading>Loading...</Loading>}
-            {!loading && data.movies && data.movies.map(m => <Movie key={ m.id } id={ m.id } />)}
+            {!loading && data.movies && (
+              <Movies>
+                {data.movies.map(m => (
+                  <Movie key={ m.id } id={ m.id } bg={ m.medium_cover_image } />
+                ))}
+              </Movies>
+            )}
         </Container>
     );
 };
