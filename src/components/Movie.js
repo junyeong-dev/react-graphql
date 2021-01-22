031c86a15e37fb921a315cfbcb58163354db6817
Jun-cloud <kjy8827@gmail.com>
2021-01-22 17:02:21 +0900

    Local state part one

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,6 @@ const Container = styled.div`
     height: 380px;
     width: 100%;
     box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-    overflow: hidden;
     border-radius: 7px;
 `;
 
@@ -18,10 +17,11 @@ const Poster = styled.div`
     background-position: center center;
 `;
 
-export default ({ id, bg }) => (
+export default ({ id, bg, isLiked }) => (
     <Container>
         <Link to={`/${ id }`}>
             <Poster bg={ bg } />
         </Link>
+        <button>{ isLiked ? "Unlike" : "Like" }</button>
     </Container>
 );
